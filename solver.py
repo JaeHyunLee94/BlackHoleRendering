a759aa2abebdaebb7b489944f0725f16d93f1f3a
jhlee <leejaehyun1223@gmail.com>
2024-11-21 21:21:01 -0600

    solving with sphere ray intersection

--- solver.py
+++ solver.py
@@ -1,10 +1,34 @@
+import numpy as np
+
+
 class Solver:
-    def __init__(self, camera):
-        self._camera = camera
+    def __init__(self, scene):
+        self._scene = scene
         pass
 
-    def set_camera(self, camera):
-        self._camera = camera
+    def set_scene(self, black_holes):
+        self._scene = black_holes
 
-    def cast_ray(self, x, y):
-        pass
+    def solve(self, ray):
+        """
+        Determine the color of the ray.
+        """
+
+        """
+        Temporal implementation ^^b
+        """
+        for bh in self._scene:
+            origin_to_center = ray.origin - bh.pos
+            a = np.dot(ray.direction, ray.direction)
+            b = 2 * np.dot(ray.direction, origin_to_center)
+            c = np.dot(origin_to_center, origin_to_center) - bh.radius ** 2
+            discriminant = b ** 2 - 4 * a * c
+
+            if discriminant > 0:
+                ray.color = np.array([0, 0, 0])
+            else:
+                pass
+
+
+
+
