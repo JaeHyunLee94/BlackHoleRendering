00c575e88eccdb542c458fddd035625df3f3363d
jhlee <leejaehyun1223@gmail.com>
2024-12-03 13:56:30 -0600

    delete spherical coord

--- ray.py
+++ ray.py
@@ -8,14 +8,14 @@ class Ray:
 
         self.color = np.array([0, 0, 0])  # initialize color to black
         # current position in spherical coordinates
-        self.r, self.theta, self.phi = self.cartesian_to_spherical(self.position)
+        # self.r, self.theta, self.phi = self.cartesian_to_spherical(self.position)
 
     def at(self, t):
         return self.position + t * self.direction
 
-    def cartesian_to_spherical(self, coord):
-        x, y, z = coord
-        r = np.linalg.norm(coord)
-        theta = np.arccos(z / r) if r != 0 else 0  # Handle zero-length vector
-        phi = np.arctan2(y, x)
-        return r, theta, phi
+    # def cartesian_to_spherical(self, coord):
+    #     x, y, z = coord
+    #     r = np.linalg.norm(coord)
+    #     theta = np.arccos(z / r) if r != 0 else 0  # Handle zero-length vector
+    #     phi = np.arctan2(y, x)
+    #     return r, theta, phi
